b49a08fab48a6517e01cf4a894edb023edc959c9
Kristian Frøhlich <186453+kfh@users.noreply.github.com>
2026-01-30 09:04:43 +0100

    Fixed kotlinx deserialization

--- edi-adapter-model/src/main/kotlin/no/nav/helsemelding/ediadapter/model/DeliveryState.kt
+++ edi-adapter-model/src/main/kotlin/no/nav/helsemelding/ediadapter/model/DeliveryState.kt
@@ -1,12 +1,20 @@
 package no.nav.helsemelding.ediadapter.model
 
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
 enum class DeliveryState(val value: String, val description: String) {
+    @SerialName("Unconfirmed")
     UNCONFIRMED("Unconfirmed", "Transport is not confirmed"),
+
+    @SerialName("Acknowledged")
     ACKNOWLEDGED("Acknowledged", "Transport is confirmed. Equivalent to 'Acknowledgement' without severe 'eb:ErrorList/eb:HighestSeverity'"),
+
+    @SerialName("Rejected")
     REJECTED("Rejected", "Transport is rejected. Equivalent to 'MessageError' with 'eb:ErrorList/eb:HighestSeverity'=\"Error\""),
+
+    @SerialName("Unknown")
     UNKNOWN("Unknown", "The value is not supported");
 
     companion object {
