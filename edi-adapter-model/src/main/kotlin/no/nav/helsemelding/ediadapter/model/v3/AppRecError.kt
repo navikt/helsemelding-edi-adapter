61a669ba4c58d27c51b973c63be09adda2d21332
Kristian Frøhlich <186453+kfh@users.noreply.github.com>
2026-09-08 17:46:21 +0200

    Linking to all relevant error codes

--- edi-adapter-model/src/main/kotlin/no/nav/helsemelding/ediadapter/model/v3/AppRecError.kt
+++ edi-adapter-model/src/main/kotlin/no/nav/helsemelding/ediadapter/model/v3/AppRecError.kt
@@ -6,6 +6,7 @@ import kotlinx.serialization.Serializable
  * An error reported by the receiving application in an application receipt.
  *
  * @property errorCode Code identifying the application error, for example `E10`.
+ *     See [code system 8221](https://finnkode.helsedirektoratet.no/adm/collections/8221?q=8221).
  * @property details Additional information about this particular error; optional when sending a receipt.
  * @property description Meaning of the error code as defined by its code system.
  * @property oid Object identifier of the code system that defines [errorCode].
